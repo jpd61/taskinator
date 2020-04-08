0356e8f38e974f4ff20e3c5b25dd08ca0ee76e0e
jpd61 <jpd@dewoodyoil.com>
2020-04-08 16:02:47 -0500

    drag styling updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -184,6 +184,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 }
 
@@ -203,7 +204,16 @@ var dropTaskHandler = function(event) {
         statusSelectEl.selectedIndex = 2;
       }
 
+    dropZoneEl.removeAttribute("style");
+    
     dropZoneEl.appendChild(draggableElement);
+}
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
 }
 
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -215,4 +225,6 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
